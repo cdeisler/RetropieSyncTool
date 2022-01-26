b818a2d4041d0a68c4c49b873ecf628eedfeb21a
cdeisler <c33jd13s3l@gmail.com>
2022-01-26 10:54:54 -0500

    cont

---
+++ RetropieSyncTool/install-remote-retropie-client.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+chmod +x retropie-get-server-random-rom.sh
+cd /opt/retropie/configs/all/ 
+nohup /bin/bash /opt/retropie/configs/all/retropie-get-server-random-rom.sh &
--- RetropieSyncTool/retropie-get-server-random-rom.sh
+++ RetropieSyncTool/retropie-get-server-random-rom.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-IP=$(curl '192.168.0.103/?emu=fba')
-echo "ip is $IP"
-/bin/bash /opt/retropie/configs/all/runRom.sh "$IP" &
+RETROPIE_VER="0"
+PIMODEL=$(cat /proc/cpuinfo | grep 'Model')
+  case $PIMODEL in
+    *"Pi 4 Model B"*)
+        RETROPIE_VER="4.40";;
+    *"Pi 3 Model"*)
+        RETROPIE_VER="2.3";;
+    *"Pi Zero 2"*)
+        RETROPIE_VER="2.3";;
+    *"Pi Zero 1"*)
+        RETROPIE_VER="0.1";;
+    *)
+        echo "INVALID PIMODEL";;
+  esac
+echo "192.168.0.103/?emu=fba&model=$PIMODEL"
+ROM=$(curl "192.168.0.103/?emu=fba&model=$RETROPIE_VER")
+echo "ROM is $ROM pi_model: $PIMODEL retropie_ver: $RETROPIE_VER"
+/bin/bash /opt/retropie/configs/all/runRom.sh "$ROM" "$PIMODEL" &
